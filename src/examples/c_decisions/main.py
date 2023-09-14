628d81bbd75f332cbd0a2d3488d8b30a5fd64775
Art Gonzalez <34781118+artgonzalez@users.noreply.github.com>
2023-09-14 00:21:00 +0000

    if elif menu example

--- src/examples/c_decisions/main.py
+++ src/examples/c_decisions/main.py
@@ -1,9 +1,4 @@
 import decisions
 
-grade = int(input("Enter a numerical grade: "))
-
-letter_grade = decisions.get_letter_grade(grade)
-
-print("Grade is: " + letter_grade)
-
+decisions.run_menu()
 
