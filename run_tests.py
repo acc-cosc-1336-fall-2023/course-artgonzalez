ad06ea31a01f1031064c362972ddf5c08486076a
Art Gonzalez <34781118+artgonzalez@users.noreply.github.com>
2023-09-28 00:34:16 +0000

    local function var globals, random, return 2 param

--- run_tests.py
+++ run_tests.py
@@ -6,7 +6,7 @@ has the test functions
 #from tests.homework.b_in_proc_out import tests_in_proc_out
 #from tests.examples.a_example import tests_devprocess
 #from tests.examples.c_decisions import tests_decisions
-from tests.examples.d_repetition import tests_repetition
+from tests.examples.e_functions import tests_functions
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_repetition)
+suite = unittest.TestLoader().loadTestsFromModule(tests_functions)
 unittest.TextTestRunner(verbosity=2).run(suite)
