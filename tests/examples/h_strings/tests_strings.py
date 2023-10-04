1205fca9cce3f955ed79059d3ad0f4fc6bc525f9
Art Gonzalez <34781118+artgonzalez@users.noreply.github.com>
2023-10-04 23:34:43 +0000

    string comparison

--- tests/examples/h_strings/tests_strings.py
+++ tests/examples/h_strings/tests_strings.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.examples.h_strings.strings import concat_strings, slice_string, slice_w_step_value, test_config
+from src.examples.h_strings.strings import concat_strings, search_in_string, slice_string, slice_w_step_value, test_config
 
 class Test_Config(unittest.TestCase):
 
@@ -19,4 +19,22 @@ class Test_Config(unittest.TestCase):
     def test_slice_w_step_value(self):
         letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         self.assertEqual(slice_w_step_value(letters), "ACEGIKMOQSUWY")
+
+    def test_compare_strings(self):
+        #Python compare ASCII numerical values in string comparisons
+        str1 = "c" #99
+        str2 = "p" #112
+
+        self.assertEqual(str1 < str2, True)
+        self.assertEqual(str2 < str1, False)
+        self.assertEqual("C++" == "C++ ", False)
+
+    
+    def test_search_in_string(self):
+        str1 = "abc"
+        str2 = "abcdef"
+
+        self.assertEqual(search_in_string(str1, str2), True)
+        self.assertEqual(search_in_string("efg", str2), False)
+
 
