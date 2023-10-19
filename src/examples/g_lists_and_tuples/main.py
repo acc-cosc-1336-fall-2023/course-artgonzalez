f1d7fcc545980bc340523c114a8b45931a297243
Art Gonzalez <34781118+artgonzalez@users.noreply.github.com>
2023-10-19 00:18:25 +0000

    tuples

--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,19 +1,6 @@
 #main program
 
-import lists
-#lists.display_list()
-#lists.display_list_w_while()
-#lists.display_reverse_list_w_while()
-#lists.display_list_w_for_range()
-#lists.display_list_w_for_range_w_step()
-#lists.display_reverse_list_w_for_range()
+import tuples
 
-#list1 = [[5, 10, 20], [6, 10, 5], [5, 0, 20]]
-#print(list1[0][0])
-#print(list1[2])
-
-#list_at_indx_0 = list1[0]
-#print(list_at_indx_0)
-
-list1 = lists.get_multiplication_table(3)
-print(list1)
+#tuples.create_tuple()
+tuples.create_list_from_tuple()
