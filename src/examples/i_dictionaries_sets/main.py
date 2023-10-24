3d363adf422a57f88681f9b422e5ba5179c0b7f2
Art Gonzalez <34781118+artgonzalez@users.noreply.github.com>
2023-10-24 00:12:27 +0000

    keys are unique

--- src/examples/i_dictionaries_sets/main.py
+++ src/examples/i_dictionaries_sets/main.py
@@ -8,9 +8,11 @@ value = input("Enter value: ")
 
 phonebook[key] = value
 
-print(phonebook[key])
-
-del phonebook[key]
-
 print(phonebook)
 
+key = input("Enter key: ")
+value = input("Enter value: ")
+
+phonebook[key] = value
+
+print(phonebook)
