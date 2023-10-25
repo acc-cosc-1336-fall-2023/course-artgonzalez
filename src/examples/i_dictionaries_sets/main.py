a6849e0eec6b2096ff4df14ba394cd90c3477228
Art Gonzalez <34781118+artgonzalez@users.noreply.github.com>
2023-10-25 23:28:47 +0000

    create sets

--- src/examples/i_dictionaries_sets/main.py
+++ src/examples/i_dictionaries_sets/main.py
@@ -1,23 +1,14 @@
 import dictionaries
-
+import sets
 #main program
-phonebook = {}#empty dictionary
 
-print('Add friends\n')
-dictionaries.add_friend_phonebook('Chris', '555-1111', phonebook)
-dictionaries.add_friend_phonebook('Katie', '555-2222', phonebook)
+myset = set('abc')
 
-for name, value in phonebook.items():
-    print(name, value)
+print (myset)
 
-print('\nUpdate katie \n')
-dictionaries.update_friend_phonebook('Katie', '555-2345', phonebook)
+myset.add('d')
 
-for name, value in phonebook.items():
-    print(name, value)
+print (myset)
 
-print('\nDelete Chris \n')
-dictionaries.delete_friend_phonebook('Chris', phonebook)
-
-for name, value in phonebook.items():
-    print(name, value)
+for item in myset:
+    print(item)
